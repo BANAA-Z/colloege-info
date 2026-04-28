2fea0b5e8790c1d71d5fcec90471403e62f79bf8
BANAA-Z <3521691602@qq.com>
2026-04-28 14:33:44 +0800

    中转

--- assets/js/cloudbase-init.js
+++ assets/js/cloudbase-init.js
@@ -1,6 +1,8 @@
 // 环境ID直接写死，不再依赖config.js
-const ENV_ID = "college-info-d8gllm2mv8ef87c41";
-
+const app = cloudbase.init({
+  env: "college-info-d8glle2mv8ef87c41", // 你的TCB环境ID
+  apiHost: "https://nameless-frog-e039.x82717006.workers.dev" // 你的Worker地址
+});
 // 新版 CloudBase SDK 初始化
 const app = cloudbase.init({ env: ENV_ID });
 const auth = app.auth();
